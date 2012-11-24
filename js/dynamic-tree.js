16f956c6d5e233d1a42dcd9f9fc32ee97f792898
snetcher <snetcher@gmail.com>
2012-11-24 17:48:04 +0200

    File commit

---
+++ js/dynamic-tree.js
@@ -0,0 +1,26 @@
+function NavAcc( container, target ) {
+	this.container = container;
+	this.target = target;
+
+	this.subm = this.container.find(this.target)
+											.hide()
+											.prev()
+											.addClass('with-submenu')
+											.on('click', function(e){
+											    $(this)
+													.next('ul')
+													.slideToggle('fast')
+													.toggleClass('open')
+													.siblings('ul:visible')
+													.slideUp('fast');
+													
+        										$(this)
+													.toggleClass('active');
+													
+            									$(this)
+													.siblings('a')
+													.removeClass('active');
+        										
+												e.preventDefault()
+										})
+}
