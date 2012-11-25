47275443a62a606187a98d980edf3b6f32897f5c
snetcher <snetcher@gmail.com>
2012-11-25 07:44:25 +0200

    Форматирование кода.

--- js/dynamic-tree.js
+++ js/dynamic-tree.js
@@ -1,26 +1,14 @@
-function NavAcc( container, target ) {
+function NavAcc(container, target) {
 	this.container = container;
 	this.target = target;
 
-	this.subm = this.container.find(this.target)
-											.hide()
-											.prev()
-											.addClass('with-submenu')
-											.on('click', function(e){
-											    $(this)
-													.next('ul')
-													.slideToggle('fast')
-													.toggleClass('open')
-													.siblings('ul:visible')
-													.slideUp('fast');
-													
-        										$(this)
-													.toggleClass('active');
-													
-            									$(this)
-													.siblings('a')
-													.removeClass('active');
-        										
-												e.preventDefault()
-										})
+	this.subm = this.container.find(this.target).hide().prev().addClass('with-submenu').on('click', function(e) {
+		$(this).next('ul').slideToggle('fast').toggleClass('open').siblings('ul:visible').slideUp('fast');
+
+		$(this).toggleClass('active');
+
+		$(this).siblings('a').removeClass('active');
+
+		e.preventDefault()
+	})
 }
